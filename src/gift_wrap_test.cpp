1b22c99f41a9a830cca92c1a56d5c25b3d26710e
Tchy258 <tchy258@yahoo.cl>
2025-06-17 00:16:01 -0400

    feat: additional arguments for setting the seed and colinear points

--- src/gift_wrap_test.cpp
+++ src/gift_wrap_test.cpp
@@ -44,20 +44,29 @@ int main(int argc, char** argv) {
 */
 ///*
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "Usage ./Test.exe <times per size> [<filename>]" << std::endl;
+    if (argc < 3) {
+        std::cout << "Usage ./Test.exe <times per size> <percentage on hull from 0 to 100> [<colinear point amount?] [seed] [<filename>]" << std::endl;
         return 1;
     }
-    auto strategy = std::unique_ptr<PointGenerationStrategy<long long>>(new HullPercentageStrategy<long long>(0.6));
+    float percentageOnHull = static_cast<float>(atoi(argv[2])) / 100.0f;
+    unsigned int colinear = 0;
+    if (argc > 3) {
+        colinear = atoi(argv[3]);
+    }
+    int seed = 123;
+    if (argc > 4) {
+        seed = atoi(argv[4]);
+    }
+    auto strategy = std::unique_ptr<PointGenerationStrategy<long long>>(new HullPercentageStrategy<long long>(percentageOnHull, colinear));
     auto hullStrategy = std::unique_ptr<GiftWrappingStrategy<long long>>(new GiftWrappingStrategy<long long>());
     auto hullStrategy2 = std::unique_ptr<GiftWrappingStrategy<long long>>(new GiftWrappingStrategy<long long>());
     HullTester<long long> tester = HullTester<long long>(std::move(strategy), std::move(hullStrategy), std::move(hullStrategy2));
     tester.setSeed(123);
     tester.setBounds(-1000, 1000);
-    std::vector<size_t> sizes({10000, 100000, 1000000});
+    std::vector<size_t> sizes({100, 1000, 10000});
     std::string filename = std::string("");
-    if (argc > 2) {
-        filename = std::string(argv[2]);
+    if (argc > 5) {
+        filename = std::string(argv[5]);
     }
     tester.timeAndTest(atoi(argv[1]), sizes, filename);
 }
