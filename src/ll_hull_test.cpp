09f64106db98d9b6750268f59c5ea49a438e000c
Tchy258 <tchy258@yahoo.cl>
2025-06-17 03:17:59 -0400

    feat: new integer test, rename long long to ll test

---
+++ src/ll_hull_test.cpp
@@ -0,0 +1,31 @@
+#include "gift_wrapping_strategy.h"
+#include <at_least_three_colinear_points_strategy.h>
+#include <hull_tester.h>
+#include <hull_percentage_strategy.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cout << "Usage ./LLHullTest.exe <times per size> <percentage on hull from 0 to 100> [<colinear point amount?] [seed] [<filename>]" << std::endl;
+        return 1;
+    }
+    float percentageOnHull = static_cast<float>(atoi(argv[2])) / 100.0f;
+    unsigned int colinear = 0;
+    if (argc > 3) {
+        colinear = atoi(argv[3]);
+    }
+    int seed = 123;
+    if (argc > 4) {
+        seed = atoi(argv[4]);
+    }
+    std::string filename = std::string("");
+    if (argc > 5) {
+        filename = std::string(argv[5]);
+    }
+    auto strategy = std::unique_ptr<PointGenerationStrategy<long long>>(new HullPercentageStrategy<long long>(percentageOnHull, colinear));
+    auto hullStrategy = std::unique_ptr<GiftWrappingStrategy<long long>>(new GiftWrappingStrategy<long long>());
+    auto hullStrategy2 = std::unique_ptr<GiftWrappingStrategy<long long>>(new GiftWrappingStrategy<long long>());
+    executeTest<long long>(atoi(argv[1]), filename, -159200LL, 159200LL, seed, std::move(strategy), std::move(hullStrategy), std::move(hullStrategy2));
+}
